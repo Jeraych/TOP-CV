9a65da7c940301dea42557feb0f2b4e1178bdfff
Jeraych <Jacky2022332467@gmail.com>
2026-01-19 23:25:11 +1300

    Setup componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  });
+
+  const [education, setEducation] = useState({
+    school: '',
+    title: '',
+    date: ''
+  });
+
+  const [experience, setExperience] = useState({
+    company: '',
+    position: '',
+    responsibilities: '',
+    from: '',
+    to: ''
+  });
+
   return <h1>TOP CV</h1>
 }
 
